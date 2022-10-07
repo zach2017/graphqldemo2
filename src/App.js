9c9901d6ecf627ef398aadd30ccf357644118585
devcmgm <zaclewis@gmail.com>
2022-10-07 11:02:17 -0500

    fullexample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql } from "@apollo/client";
 import './App.css';
+
+const FILMS_QUERY = gql`
+  {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
     <div className="App">
-  
+      <ApolloProvider client={client}>
+        <Gql1/>
+      </ApolloProvider>,
+    </div>
+  );
+}
+
+export function Gql1() {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>
+
+  return (
+    <div>
+      <h1>SpaceX Launches</h1>
+      <ul>
+        {data.launchesPast.map((launch) => (
+          <li key={launch.id}>{launch.mission_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
